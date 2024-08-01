974351654117f2bc1c246b36f8ec17c8c494bda5
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-01 20:19:07 +0000

    concat-str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function to concatenate strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Annotateed function to concat strings
+    """
+    return (str1+str2)
