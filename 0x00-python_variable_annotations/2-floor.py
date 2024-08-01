5c668d482e69693a893c9963bd9335c95a0c1032
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-01 20:33:44 +0000

    floor-float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Function to floor float"""
+from math import floor
+
+
+def floor(n: float) -> int:
+    """ Annotated method to floor a number
+    """
+    return int(n // 1)
