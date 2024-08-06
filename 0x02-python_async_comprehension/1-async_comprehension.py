852b6c41c9c85431acd6288b6ddcf256d15e0e23
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-06 19:18:29 +0000

    task2

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" async list comprehension task2"""
+import asyncio
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+async def async_comprehension() -> List[float]:
+    """ Async list comprehension function
+    """
+    return [n async for n in async_generator()]
